10f9acd3bbdba1a41ee2ca71b20ed9ec965d35e1
Nicolae Cleju <nikcleju@gmail.com>
2021-03-30 14:25:17 +0300

    Better model saving

--- DKSVD_train_model.py
+++ DKSVD_train_model.py
@@ -1,7 +1,9 @@
 """
 """
 
+import datetime
 import numpy as np
+import os
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
@@ -62,9 +64,6 @@ dataloader_train = DataLoader(
 
 # Create a file to see the output during the training:
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-file_to_print = open("results_training.csv", "w")
-file_to_print.write(str(device) + "\n")
-file_to_print.flush()
 
 # Initialization:
 patch_size = 8
@@ -109,8 +108,43 @@ epochs = 3
 running_loss = 0.0
 
 print_every = 100
-save_every_print = 20
+save_every_print = 60
 
+# Save everything in this folder
+save_folder = 'out'
+os.makedirs(save_folder, exist_ok=True)
+
+# Set save file path
+file_to_print_name_template = os.path.join(save_folder, "results_training_{}.csv")
+i = 1
+while os.path.exists(file_to_print_name_template.format(i)):
+    i += 1
+file_to_print_name = file_to_print_name_template.format(i)
+
+# Initialize starting params
+epoch_start = 0
+i_start = 0
+
+# Load from previous file
+start_training_from = None   # checkpoint
+#start_training_from = "out/checkpoint_epoch1_iter200_trainloss0.0195154356_testloss0.0184636429.pth.tar"
+if start_training_from is not None:
+    checkpoint = torch.load(start_training_from)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    epoch_start = checkpoint['epoch']
+    i_start = checkpoint['current_iter']
+
+# Write header info
+file_to_print = open(file_to_print_name, "w")
+file_to_print.write(str(datetime.datetime.now()) + "\n")
+file_to_print.write(str(device) + "\n")
+file_to_print.write("start_training_from: " + str(start_training_from) + "\n")
+file_to_print.write("epoch_start: " + str(epoch_start) + "\n")
+file_to_print.write("i_start: " + str(i_start) + "\n")
+file_to_print.flush()
+
+# Train
 train_losses, test_losses = [], []
 for epoch in range(epochs):  # loop over the dataset multiple times
     for i, (sub_images, sub_images_noise) in enumerate(dataloader_train, 0):
@@ -132,15 +166,20 @@ for epoch in range(epochs):  # loop over the dataset multiple times
         # print statistics
         running_loss += loss.item()
         if i % print_every == print_every - 1:  # print every x mini-batches
-            train_losses.append(running_loss / print_every)
+
+            # Compute train_loss current
+            train_loss = running_loss / print_every
+
+            train_losses.append(train_loss)
 
             end = time.time()
             time_curr = end - start
             file_to_print.write("time:" + " " + str(time_curr) + "\n")
             start = time.time()
 
-            file_to_print.write("norm of Dict - Identity = {}\n".format( torch.norm( model.Dict.cpu() @  model.Dict.cpu().t() - torch.eye(64,64) ) ))
+            #file_to_print.write("norm of Dict - Identity = {}\n".format(  ))
 
+            # Compute test loss
             with torch.no_grad():
                 test_loss = 0
 
@@ -154,19 +193,21 @@ for epoch in range(epochs):  # loop over the dataset multiple times
                     test_loss += loss.item()
 
                 test_loss = test_loss / len(dataloader_test)
+            test_losses.append(test_loss)
 
             end = time.time()
             time_curr = end - start
             file_to_print.write("time:" + " " + str(time_curr) + "\n")
             start = time.time()
 
-            test_losses.append(test_loss)
-            s = "[%d, %d, batchnum=%d] loss_train: %f, loss_test: %f" % (
+            # Print info in file
+            s = "[%d, %d, batchnum=%d] loss_train: %f, loss_test: %f, norm of DDT-I: %f" % (
                 epoch + 1,
                 (i + 1) * batch_size,
                 (i + 1),
-                running_loss / print_every,
+                train_loss,
                 test_loss,
+                torch.norm( model.Dict.cpu() @  model.Dict.cpu().t() - torch.eye(64,64) )
             )
             s = s + "\n"
             file_to_print.write(s)
@@ -175,12 +216,29 @@ for epoch in range(epochs):  # loop over the dataset multiple times
 
         #if i % (10 * print_every) == (10 * print_every) - 1:
         if i % (save_every_print * print_every) == (save_every_print * print_every) - 1:
-            model_name  = "model_{}.pth".format(i+1)
-            losses_name = "losses_{}.npz".format(i+1)
-            torch.save(model.state_dict(), model_name)
-            np.savez(
-                losses_name, train=np.array(test_losses), test=np.array(train_losses)
-            )
+            model_name  = "model_epoch{}_iter{}_trainloss{:.10f}_testloss{:.10f}.pth".format(epoch+1, i+1, train_loss, test_loss)
+            model_savefile = os.path.join(save_folder, model_name)
+
+            checkpoint_name  = "checkpoint_epoch{}_iter{}_trainloss{:.10f}_testloss{:.10f}.pth.tar".format(epoch+1, i+1, train_loss, test_loss)
+            checkpoint_savefile = os.path.join(save_folder, checkpoint_name)
+
+            # Save model only, for inference
+            torch.save(model.state_dict(), model_savefile)
+
+            #losses_name = "losses_{}.npz".format(i+1)
+            # np.savez(
+            #     losses_name, train=np.array(test_losses), test=np.array(train_losses)
+            # )
+
+            # Checkpoint everything, for subsequent training
+            torch.save({
+                            'epoch': epoch,
+                            'model_state_dict': model.state_dict(),
+                            'optimizer_state_dict': optimizer.state_dict(),
+                            'dataloader_train': dataloader_train,
+                            'current_iter': i+1   # next iteration to be run
+                        }, 
+                        checkpoint_savefile)
 
 
 file_to_print.write("Finished Training")
